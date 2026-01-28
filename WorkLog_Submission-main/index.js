e1b6b5e03611940aaf211836b65f048d9e3676c9
rohanreddy-lgtm <shankurirohan76@gmail.com>
2026-01-28 12:21:06 +0530

    Update email address in form submission

--- WorkLog_Submission-main/index.js
+++ WorkLog_Submission-main/index.js
@@ -24,7 +24,7 @@ async function doFill() {
     await page.goto('https://forms.zohopublic.in/Kalvium/form/Signup/formperma/GeJFMLBDfoWlIJfhI46Qyx0Dlf3kHhMSRsvMItq_Riw');
 
     await page.keyboard.press('Tab');
-    await page.keyboard.type('vijaykumar.r.s67@kalvium.community');
+    await page.keyboard.type('shankuri.rohan.s67@kalvium.community');
 
     await new Promise(resolve => setTimeout(resolve, 2000));
     await page.keyboard.press('Tab');
